c49d1a765cd7539f917b57ec4ac525b48567fe14
Ben Kiggen <benkiggen@gmail.com>
2018-08-17 09:01:34 -0700

    return range of numbers up to specified number

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,31 @@
 //global variables
 var numberInput;
-
+var numberOutput = [];
 
 //user interface logic
 $(document).ready(function(){
   $("#input-form").submit(function(event){
     event.preventDefault();
     numberInput = $("#number-input").val();
-  })
+    numberProcess();
+  });
 });
 
 
 //business logic
 
-for(var i = 0; i > numberInput; i++){
-  
+function numberProcess() {
+  for(var i = 0; i <= numberInput; i++){
+    if (i % 3 === 0){
+      
+    } else if {
+
+    } else {
+
+    } else {
+      console.log("WHAT'S WRONG?")
+    }
+    numberOutput.push(i);
+
+  };
 }
