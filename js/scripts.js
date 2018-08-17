de7a2f37a631f90e52e23e13919ec0ea20667a93
Ben Kiggen <benkiggen@gmail.com>
2018-08-17 09:20:36 -0700

    business logic complete

--- js/scripts.js
+++ js/scripts.js
@@ -17,28 +17,15 @@ $(document).ready(function(){
 function numberProcess() {
   for(var i = 0; i <= numberInput; i++){
     if (i % 3 === 0 && i != 0){
-
       numberOutput.push("I'm sorry, Dave. I'm afraid I can't do that.");
-      console.log(i + "multiple of 3");
-
     } else {
-
       if (i.toString().indexOf('1') > -1){
-        console.log(i + "its got a 1, boy");
         numberOutput.push("Boop!");
-
       } else if (i.toString().indexOf('0') > -1){
         numberOutput.push("Beep!");
       } else {
         numberOutput.push(i);
       }
     };
-
-
-
-
-
-    // numberOutput.push(i);
-
   };
 }
