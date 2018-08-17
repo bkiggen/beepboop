940a34b09ebbb4a809b0e952abe8b00d07fef331
Ben Kiggen <benkiggen@gmail.com>
2018-08-17 09:14:24 -0700

    correctly recognize multiples of 3

--- js/scripts.js
+++ js/scripts.js
@@ -17,14 +17,22 @@ $(document).ready(function(){
 function numberProcess() {
   for(var i = 0; i <= numberInput; i++){
     if (i % 3 === 0){
+
       numberOutput.push("I'm sorry, Dave. I'm afraid I can't do that.");
-      console.log("multiple of 3");
+      console.log(i + "multiple of 3");
+
     } else {
-      if (i.toString().match(/1*/g)){
-        console.log("its got a 1, boy");}
-      // } else if (i.toString.match(/0/g)){
-      //   console.log("it's got a 0, boy");
-      // }
+
+      if (i.toString().match(/1*/)){
+        console.log(i + "its got a 1, boy");
+        numberOutput.push("Boop!");
+
+      } else if (i.toString().match(/0*/)){
+        console.log("it's got a 0, boy");
+        numberOutput.push("Beep!");
+      } else {
+        numberOutput.push(i);
+      }
     };
 
 
